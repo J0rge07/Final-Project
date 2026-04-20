272bf873fdf6745a35993ed33c242aaff39359ac
Stefany <sa0013@morningside.edu>
2026-04-20 14:28:37 -0500

    first changes stefany

--- peripheral.main.cpp
+++ peripheral.main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <BLE.h>
 #include <Adafruit_LSM6DSOX.h>
+#include <I2S.h> // for the speaker 
+
 
 const char *myserviceuuid = "3687d5e6-22a7-4b4d-a1e6-60d516e63649";
 const char *strDatauuid   = "34e0e5b7-1a4a-4ea6-b0c8-df0f50e8a0ce";
